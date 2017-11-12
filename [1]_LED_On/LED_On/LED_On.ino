8a449938d1cfee77d67709469b4b01916e4b034f
krittinunt <krittinunt@gmail.com>
2017-11-13 00:06:17 +0700

    Add files via upload

---
+++ [1]_LED_On/LED_On/LED_On.ino
@@ -0,0 +1,13 @@
+/* Author : Mr.KRITTINUNT CHOBTRONG */
+
+#define LED_1     22
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_1, OUTPUT);
+  digitalWrite(LED_1, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
