cb7840ad43e676e079e17856ff89bada7c85bee3
krittinunt <krittinunt@gmail.com>
2018-02-12 08:36:17 +0700

    Add files via upload

---
+++ [22]_R3_LCD_4bits/R3_LCD_4bits/R3_LCD_4bits.ino
@@ -0,0 +1,319 @@
+#define LCD_E          12
+#define LCD_RW         11
+#define LCD_RS         10
+
+#define LCD_D7         9
+#define LCD_D6         8
+#define LCD_D5         7
+#define LCD_D4         6
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LCD_E, OUTPUT);
+  pinMode(LCD_RW, OUTPUT);
+  pinMode(LCD_RS, OUTPUT);
+  pinMode(LCD_D7, OUTPUT);
+  pinMode(LCD_D6, OUTPUT);
+  pinMode(LCD_D5, OUTPUT);
+  pinMode(LCD_D4, OUTPUT);
+  
+  // set pin E, RW, RS to logic 0
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RW, LOW);
+  digitalWrite(LCD_RS, LOW);
+  
+  // Wait 20mSec for LCD power up
+  delay(20);
+   
+  // Function Set
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, HIGH);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+    // Clear Display
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  // Display/Cursor
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, HIGH);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  // Entry Mode Set
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  // Home Display
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  // print T to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+  
+  // print e to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+  
+  // print s to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+  
+  // print t to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+  
+  // Set DD-RAM Address to LCD line 2
+  digitalWrite(LCD_D7, HIGH);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  // print L to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, HIGH);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+  
+  // print C to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, LOW);
+  digitalWrite(LCD_D5, HIGH);
+  digitalWrite(LCD_D4, HIGH);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+  
+  // print D to LCD
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_RS, HIGH);
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  delay(5);
+  
+  digitalWrite(LCD_D7, LOW);
+  digitalWrite(LCD_D6, HIGH);
+  digitalWrite(LCD_D5, LOW);
+  digitalWrite(LCD_D4, LOW);
+  
+  digitalWrite(LCD_E, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(LCD_E, LOW);
+  digitalWrite(LCD_RS, LOW);
+  delay(5);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ [24]_R3_LCD_4bitsUseLibCGRAM/R3_LCD_4bitsUseLibCGRAM/R3_LCD_4bitsUseLibCGRAM.ino
@@ -0,0 +1,87 @@
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+//                  RS, E, D4,D5,D6,D7
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(10, 12, 6, 7, 8, 9);
+
+byte bar_0[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+byte bar_1[8] = {0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10};
+byte bar_2[8] = {0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18};
+byte bar_3[8] = {0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C};
+byte bar_4[8] = {0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E};
+byte bar_5[8] = {0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F};
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.createChar(0, bar_0);
+  lcd.createChar(1, bar_1);
+  lcd.createChar(2, bar_2);
+  lcd.createChar(3, bar_3);
+  lcd.createChar(4, bar_4);
+  lcd.createChar(5, bar_5);
+  // Print a message to the LCD.
+  lcd.setCursor(0, 0);
+  lcd.print("<>?()");
+  lcd.setCursor(0, 1);
+  lcd.print("~~!! ^_^ ++/*/");
+  
+  lcd.setCursor(9, 0);
+  
+  lcd.write((uint8_t)0);
+  lcd.write((uint8_t)1);
+  lcd.write((uint8_t)2);
+  lcd.write((uint8_t)3);
+  lcd.write((uint8_t)4);
+  lcd.write((uint8_t)5);
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  //lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  //lcd.print(millis() / 1000);
+}
+
